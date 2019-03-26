1cde00e182bfb5567a2f5aae013cddf188c89f90
Timofey Samsonov <tsamsonov@geogr.msu.ru>
2019-03-26 16:44:46 +0300

    add C++ width class

---
+++ WidthCalculator.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Timofey Samsonov on 2019-03-26.
+//
+
+#include "WidthCalculator.h"
---
+++ WidthCalculator.h
@@ -0,0 +1,14 @@
+//
+// Created by Timofey Samsonov on 2019-03-26.
+//
+
+#ifndef GENERALIZE_DEM_WIDTHCALCULATOR_H
+#define GENERALIZE_DEM_WIDTHCALCULATOR_H
+
+
+class WidthCalculator {
+
+};
+
+
+#endif //GENERALIZE_DEM_WIDTHCALCULATOR_H
